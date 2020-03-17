a75c852bcf6e95590e5cef6f517384a78466c043
tktezy <tkey5074@gmail.com>
2020-03-17 20:26:51 +0300

    Second task solution

--- HW01/Igor_Hrybinyk/igor_hrybinyk_hw_01_t_03.rb
+++ HW01/Igor_Hrybinyk/igor_hrybinyk_hw_01_t_03.rb
@@ -1,1 +1,10 @@
 # 3. Get the number 2009 using arithmetic and eleven twos
+
+def get_number(result)
+  if 2222-222 + 22 / 2 - 2 == result
+    puts "Well done, #{result} == 2009"
+  end
+end
+
+get_number(2009)
+
