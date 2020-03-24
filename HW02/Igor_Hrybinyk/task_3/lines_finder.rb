747783169b9b3322c8b22d1c0e9ca04ef1157d62
tktezy <tkey5074@gmail.com>
2020-03-24 15:26:26 +0300

    Task 3: added script witch find duration of the action in seconds

--- HW02/Igor_Hrybinyk/task_3/lines_finder.rb
+++ HW02/Igor_Hrybinyk/task_3/lines_finder.rb
@@ -7,7 +7,7 @@ class LinesFinder
 
   LINES_FORMAT = /[cC]alling\score\swith\saction/.freeze
 
-  def find_match_lines
+  def find_matching_lines
     lines.select { |line| line.match?(LINES_FORMAT) }
   end
 end
