904b45dc608f77d50e960ae36e71f3f730a6adc4
tktezy <tkey5074@gmail.com>
2020-03-29 01:44:14 +0300

    Added ability to notify mentor. Better names

--- HW03/Igor_Hrybinyk/app.rb
+++ HW03/Igor_Hrybinyk/app.rb
@@ -4,14 +4,14 @@ require_relative 'homework.rb'
 require_relative 'notifications.rb'
 
 # 1) Create mentor
-mentor = Mentor.new('Jack Gonsales')
+mentor = Mentor.new('Jack')
 puts "#{mentor.name} (mentor):"
 
 # 2) Mentor creates a homework
 Homework.new(mentor).add_homework
 
 # 3) Create student
-student = Student.new('Ruby Hodson')
+student = Student.new('Ruby')
 
 # 4) Mentor notify student about hw
 Notifications.instance.notify_student
@@ -21,9 +21,11 @@ puts "#{student.name} (student):"
 Notifications.instance.student_notifications
 
 # 6) Student do smth with homework
-read_hw = Homework.new(student)
-read_hw.add_hw_solution
+Homework.new(student).add_hw_solution
+Notifications.instance.notify_mentor
+
 # 7) Mentor checks student notifications
+Notifications.instance.mentor_notifications
 
 # 8) Mentor write some review
 
